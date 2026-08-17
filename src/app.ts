260309e51194fd3fd3b9303f98c38f1ef395c366
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-08-17 11:30:28 +0100

    feat: implementa rota GET /tasks (listar todas)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Express } from 'express';
 import cors from 'cors';
+import tasksRoutes from './routes/tasks.routes.js';
 
 class App {
   server: Express;
@@ -8,11 +9,16 @@ class App {
   constructor() {
     this.server = express();
     this.middlewares();
+    this.router();
   }
 
   middlewares() {
     this.server.use(express.json());
     this.server.use(cors());
+  }
+
+  router() {
+    this.server.use('/tasks', tasksRoutes);
   }
 }
 
