b8cf77b406baafaf21e65c6dc39408cd596202af
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-08-17 12:23:32 +0100

    feat: criando o service e o controller da rota post

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -10,3 +10,18 @@ export async function listarTasks(req: Request, res: Response) {
     res.status(500).json({ erro: 'Erro ao buscar tasks' });
   }
 }
+
+export async function criarTask(req: Request, res: Response) {
+  try {
+    const { titulo, descricao } = req.body;
+
+    if (!titulo) {
+      return res.status(400).json({ erro: 'O campo título é obrigatório' });
+    }
+    const novaTask = await tasksService.criarTask(titulo, descricao);
+    res.status(201).json(novaTask);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: 'Erro ao criar task' });
+  }
+}
--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -4,3 +4,11 @@ export async function buscarTodas() {
   const resultado = await pool.query('SELECT * FROM tasks');
   return resultado.rows;
 }
+
+export async function criarTask(titulo: string, descricao: string) {
+  const resultado = await pool.query(
+    'INSERT INTO tasks (titulo, descricao) VALUES ($1, $2) RETURNING *',
+    [titulo, descricao]
+  );
+  return resultado.rows[0];
+}
