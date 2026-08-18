6ea00c04d74aa615b42f2fe2539d49290a259e65
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-08-18 16:19:07 +0100

    refactor: converte funcoes do service para arrow function

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,37 +1,37 @@
 import pool from '../config/db.js';
 
-export async function buscarTodas() {
+export const buscarTodas = async () => {
   const resultado = await pool.query('SELECT * FROM tasks');
   return resultado.rows;
-}
-export async function buscarPorId(id: string) {
+};
+export const buscarPorId = async (id: string) => {
   const resultado = await pool.query('SELECT * FROM tasks WHERE id = $1', [id]);
   return resultado.rows[0];
-}
-export async function criarTask(titulo: string, descricao: string) {
+};
+export const criarTask = async (titulo: string, descricao: string) => {
   const resultado = await pool.query(
     'INSERT INTO tasks (titulo, descricao) VALUES ($1, $2) RETURNING *',
     [titulo, descricao]
   );
   return resultado.rows[0];
-}
+};
 
-export async function editarTask(
+export const editarTask = async (
   id: string,
   titulo: string,
   descricao: string
-) {
+) => {
   const resultado = await pool.query(
     'UPDATE tasks SET titulo = $1, descricao = $2 WHERE id = $3 RETURNING *',
     [titulo, descricao, id]
   );
   return resultado.rows[0];
-}
+};
 
-export async function excluirTask(id: string) {
+export const excluirTask = async (id: string) => {
   const resultado = await pool.query(
     'DELETE FROM tasks WHERE id = $1 RETURNING *',
     [id]
   );
   return resultado.rows[0];
-}
+};
